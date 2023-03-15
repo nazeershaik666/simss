060ab320296d3c25ab0aefc337fe56d2e78c091a
nazeershaik666 <121153446+nazeershaik666@users.noreply.github.com>
2023-03-15 20:45:32 +0530

    Update account.js

--- src/emails/account.js
+++ src/emails/account.js
@@ -35,7 +35,7 @@ client
 
 const sendWelcomeEmail =(email)=>{
     const welcomeMsg={
-        from:"studentinternship1234@gmail.com",
+        from:"mailtrap@naveenrio.me",
         to:email,
         subject:"Welcome To Internship Recuritment Portal",
         html:"<div><h1>welcome to intership portal</h1><br/><p>Thanks for creating the account in our portal we hope that you will like our services.Fell free to email us if you need any help</p></br><h3>Thank you!</h3></div>"
@@ -71,7 +71,7 @@ const sendWelcomeEmail =(email)=>{
 
 const sendPasswordEmail=(email,link)=>{    
     const passwordMsg={
-        from:"mailtrap@shashank.me",
+        from:"mailtrap@naveenrio.me",
         to:email,
         subject:"Password reset mail",
         html:`<div><h4>Hi, Here is the link for reset password for your account in student internship portal\n kindly reset the password\n Note:The link is only vaild for fifteen minutes after that the link expries</h4><a href=${link}>Password reset link</a>`
@@ -99,7 +99,7 @@ const sendPasswordEmail=(email,link)=>{
 const sendOtp=(otpemail,randomdigit)=>{  
   let ok = true
   const otp={
-      from:"mailtrap@shashank.me",
+      from:"mailtrap@naveenrio.me",
       to:otpemail,
       subject:`${randomdigit} is the OTP`,
       html:`<div><h4>Hi, Here is the One Time Password : <b>${randomdigit}</b> to login into your account in student internship portal <br> \n kindly enter the OTP\n <br> <br> Note:The OTP is only vaild for fifteen minutes after that the OTP expries</h4>`
@@ -130,7 +130,7 @@ return a1()
 const sendReview=(name,mail,msg,rating)=>{
   let ok = true
   const rvw={
-    from:"mailtrap@shashank.me",
+    from:"mailtrap@naveenrio.me",
     to:"chennushashank509@gmail.com",
     subject:`New review from ${name}`,
     html:`<div><h4>Hi, Here is the new review from : <b>${name} with mail ${mail}, <br> <br> Message: ${msg} </b> <br><br> \n <b>Rating: ${rating}</b> \n <br> <br> </h4>`
