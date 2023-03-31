c49fcfbff9718882ee32431c7d90383b7a55cb7b
nazeershaik666 <121153446+nazeershaik666@users.noreply.github.com>
2023-03-31 20:07:58 +0530

    Add files via upload

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-mongoose.connect( 'mongodb+srv://naveen:naveen123@sims.brhix31.mongodb.net/?retryWrites=true&w=majority' // 'mongodb://127.0.0.1:27017/student-internship-api'  //
+mongoose.connect( 'mongodb+srv://naveen:naveen123@sims.brhix31.mongodb.net/?retryWrites=true&w=majority' // 'mongodb://127.0.0.1:27017/student-internship-api'  // 
 );
  
 
