8993762fbbfd43794043768a127c2260dbc43bc1
nazeershaik666 <121153446+nazeershaik666@users.noreply.github.com>
2023-04-07 20:50:47 +0530

    Update viewStudents.js

--- public/admin/js/viewStudents.js
+++ public/admin/js/viewStudents.js
@@ -127,7 +127,7 @@ var users = result.filter( function(item) {
   }
  for (var key in filter) {
   
-   if ( item[key].toString().toLowerCase() != filter[key] )
+   if ( item[key].toString().toLowerCase() != filter[key].toString().toLowerCase() )
   
      return false;
 
